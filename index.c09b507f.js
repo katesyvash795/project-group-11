8b13a135393c5fbb440e6acb3ae15d727a5ac7c3
yaroslawv-nexus <124203359+yaroslawv-nexus@users.noreply.github.com>
2023-08-04 07:29:17 +0000

    Deploying to gh-pages from @ katesyvash795/project-group-11@682131eb99576507c2a6558464162b37e3a8c5ee 🚀

---
+++ index.c09b507f.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector(".scroll-up-btn");window.addEventListener("scroll",(function(){window.scrollY>1e3?o.style.display="block":o.style.display="none"})),o.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})}))}();
+//# sourceMappingURL=index.c09b507f.js.map
