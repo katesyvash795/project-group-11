bc4f4ee63efbfc8b9ca5c253fa07f3f273f448b7
yaroslawv-nexus <124203359+yaroslawv-nexus@users.noreply.github.com>
2023-08-04 13:55:16 +0300

    addTitle

--- src/js/renderSelectCategory.js
+++ src/js/renderSelectCategory.js
@@ -1,6 +1,6 @@
 import { getCategory } from './bookAPI';
 import "../css/renderSelectCategory.css";
-const contRef = document.querySelector(`.category-list`);
+const contRef = document.querySelector(`.category-list-cont`);
 
 
 export function onClickCategory(category) {
@@ -20,12 +20,34 @@ function renderCategory(mark) {
 
 
 function getMarkCategory(array) {
-    return array.map(book => {
+
+    const categoryName = markLastWord(array[0].list_name);
+    const title = `<h2 class="title-category-list">${categoryName}</h2>`;
+    const elements = array.map(book => {
         return `<li class="category-item" data-id="${book._id}">
         <img class="category-item-img" src="${book.book_image} "/>
         <p class="category-item-title">${book.title}</p>
         <p class="category-item-author">${book.author}</p>
         </li>`
     }).join("");
+    console.log("hi");
+    return title + `<ul class="category-list">${elements}</ul>`;
 }
 
+
+
+
+function markLastWord(string) {
+    const categoryName = string.split(' ');
+    const quantityWord = categoryName.length;
+    let categoryNameAddSpan = "";
+    if (quantityWord > 1) {
+        for (let i = 0; i <= quantityWord - 2; i += 1) {
+            categoryNameAddSpan += " " + categoryName[i];
+        }
+        categoryNameAddSpan += " " + `<span class=last-word-catteg-item>${categoryName[quantityWord - 1]}<span>`;
+    } else {
+        categoryNameAddSpan = `<span class=last-word-catteg-item>${categoryName[0]}<span>`;
+    }
+    return categoryNameAddSpan;
+}
