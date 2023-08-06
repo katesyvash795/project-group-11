96312f3ececf913cc51b66676cbf0d9407436848
Andrii1308 <123685813+Andrii1308@users.noreply.github.com>
2023-08-07 01:16:08 +0300

    in rewriting process

--- src/js/header.js
+++ src/js/header.js
@@ -1,20 +1,14 @@
 (() => {
-  const refs = {
-    openMenuBtn: document.querySelector('[data-menu-open]'),
-    closeMenuBtn: document.querySelector('[data-menu-close]'),
-    closeMenuLnk1: document.querySelector('[link1-menu-close]'),
-    closeMenuLnk2: document.querySelector('[link2-menu-close]'),
-    closeMenuLnk3: document.querySelector('[link3-menu-close]'),
-    menu: document.querySelector('[data-menu]'),
-  };
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
 
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuLnk1.addEventListener('click', toggleMenu);
-  refs.closeMenuLnk2.addEventListener('click', toggleMenu);
-  refs.closeMenuLnk3.addEventListener('click', toggleMenu);
+  menuBtnRef.addEventListener('click', () => {
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
-  function toggleMenu() {
-    refs.menu.classList.toggle('is-hidden');
-  }
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('is-open');
+  });
 })();
