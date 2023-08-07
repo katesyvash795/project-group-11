9ff5bb6a6ab4c5ca3a85d3cf6cbcd36e17cfe29d
klytvinenko <58306149+klytvinenko@users.noreply.github.com>
2023-08-07 19:46:53 +0300

    commit li caps

--- src/js/book-list.js
+++ src/js/book-list.js
@@ -3,20 +3,20 @@ const list = document.querySelector('.book-list-ul');
 const item = list.querySelectorAll('.book-list-item');
 
 
-console.log(list);
-console.log(item);
+
 
 
 list.addEventListener('click', addClass);
 
 function addClass (evt) {
     evt.preventDefault();
+    if(!evt.target.tagName === "LI") {
+        return
+    }
     const removeEl = document.querySelector('.current');
     removeEl.classList.remove('current');
     const currentEl = evt.target;
     currentEl.classList.add('current');
-
-    console.log(currentEl);
 
     
 
