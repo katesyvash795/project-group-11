5a564a0e22ae72d1c01c13933d1baa92496722bf
yaroslawv-nexus <124203359+yaroslawv-nexus@users.noreply.github.com>
2023-08-07 13:56:08 +0000

    Deploying to gh-pages from @ katesyvash795/project-group-11@7c2b25b242037192dd0b5eff6dcc1e252c216b45 🚀

--- index.0abeab4f.js
+++
@@ -1,2 +0,0 @@
-const o=document.querySelector(".scroll-up-btn");window.addEventListener("scroll",(function(){window.scrollY>1e3?o.style.display="block":o.style.display="none"})),o.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})}));
-//# sourceMappingURL=index.0abeab4f.js.map
