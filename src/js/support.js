f95ee5769d88f2cea9f85516d48bfa7875a72e93
Julipusik <yuliuaboyko@gmail.com>
2023-08-07 14:54:46 +0300

    support updated

--- src/js/support.js
+++ src/js/support.js
@@ -1,11 +1,15 @@
 import Swiper from '../../node_modules/swiper/swiper-bundle.min.mjs';
 import '../../node_modules/swiper/swiper-bundle.css';
 
-const swiper = new Swiper('.swiper', {
+const swiperEl = document.querySelector(".swiper");
+const swipeDownBtn = document.querySelector(".support-btn");
+
+const swiper = new Swiper(swiperEl, {
   direction: 'vertical',
+  rewind: true,
+  loopSlides: 9,
   navigation: {
-    nextEl: 'support-btn',
-    prevEl: 'support-btn swipe-up',
+    nextEl: swipeDownBtn,
   },
   slidesPerView: 4,
   spaceBetween: 20,
@@ -16,3 +20,4 @@ const swiper = new Swiper('.swiper', {
     },
   },
 });
+
