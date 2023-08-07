c405fe260eb1a2bede4229bf633cc2cbfd640bdd
maksymnl <adw0988245131@gmail.com>
2023-08-07 11:41:23 +0300

    remove LS

--- src/js/localstorage.js
+++ src/js/localstorage.js
@@ -1,17 +1,31 @@
 const localStorageBtn = document.querySelector('.local-storage');
+const removeLsBtn = document.querySelector('.remove-local-storage')
 
 localStorageBtn.addEventListener('click', handleraddLs);
+removeLsBtn.addEventListener('click', handlerremoveLs)
 
 const BOOK_LS_KEY = 'book';
 
 function handleraddLs() {
-  if (!evt.target.classList.contains('js-add')) {
-    return;
-  }
-  const books = JSON.parse(localStorage.getItem(BOOK_LS_KEY)) ?? [];
-  const book = evt.target.closest('js-product');
-  const bookId = Number(book.dataset.id);
-  const currenBook = books.find(({ id }) => id === bookId);
-  book.push(currenBook);
-  localStorage.setItem(BOOK_LS_KEY, JSON.stringify(books));
+ if (!evt.target.classList.contains('js-add')) {
+ return;
+ }
+ const books = JSON.parse(localStorage.getItem(BOOK_LS_KEY)) ?? [];
+ const book = evt.target.closest('js-product');
+ const bookId = book.dataset.id;
+ const currenBook = books.find(({ id }) => id === bookId);
+ book.push(currenBook);
+ localStorage.setItem(BOOK_LS_KEY, JSON.stringify(books));
 }
+
+function handlerremoveLs() {
+    if (!evt.target.classList.contains('js-remove')) {
+    return;
+    }
+    const books = JSON.parse(localStorage.getItem(BOOK_LS_KEY)) ?? [];
+    const book = evt.target.closest('js-product');
+    const bookId = book.dataset.id;
+    const currenBook = books.find(({ id }) => id === bookId);
+    book.push(currenBook);
+    localStorage.setItem(BOOK_LS_KEY, JSON.stringify(books));
+   }
