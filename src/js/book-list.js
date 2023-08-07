eb804323b230d07186c3c37382181308b746bbf0
klytvinenko <58306149+klytvinenko@users.noreply.github.com>
2023-08-07 22:30:42 +0300

    test markup

--- src/js/book-list.js
+++ src/js/book-list.js
@@ -1,9 +1,22 @@
 
+import getCategotyList from '.js/bookAPI.js'
+
 const list = document.querySelector('.book-list-ul');
+
+// function createMarkup (list) {
+//     return list 
+//     .map(({})) => {
+//         return ` <li class="book-list-item">
+//                      <a href="" class="book-list-link"><span class="text current">${}</span></a>
+//                 </li>`
+//     }
+//     .join('');
+// }
+// const markup = createMarkup(getCategotyLis());
+// list.insertAdjacentHTML = ('beforeend', markup);
+
+
 const item = list.querySelectorAll('.book-list-item');
-
-
-
 
 
 list.addEventListener('click', addClass);
@@ -21,3 +34,5 @@ function addClass (evt) {
     
 
 }
+
+
