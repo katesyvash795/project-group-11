92faea45445ed36e9a1ba0b4d945d939c25af57c
Julipusik <yuliuaboyko@gmail.com>
2023-08-07 12:02:13 +0300

    support

--- src/js/support.js
+++ src/js/support.js
@@ -4,8 +4,8 @@ import '../../node_modules/swiper/swiper-bundle.css';
 const swiper = new Swiper('.swiper', {
   direction: 'vertical',
   navigation: {
-    nextEl: 'swiper-button-next',
-    prevEl: 'swiper-button-next swipe-up',
+    nextEl: 'support-btn',
+    prevEl: 'support-btn swipe-up',
   },
   slidesPerView: 4,
   spaceBetween: 20,
