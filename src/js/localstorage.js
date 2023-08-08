5549a123fe727d51b93b94b0382061d23779062f
maksymnl <adw0988245131@gmail.com>
2023-08-08 09:26:30 +0300

    внес изменения в ф-ю
    не работает

--- src/js/localstorage.js
+++ src/js/localstorage.js
@@ -6,26 +6,26 @@ removeLsBtn.addEventListener('click', handlerremoveLs)
 
 const BOOK_LS_KEY = 'book';
 
-function handleraddLs() {
+function handleraddLs(evt) {
  if (!evt.target.classList.contains('js-add')) {
  return;
  }
  const books = JSON.parse(localStorage.getItem(BOOK_LS_KEY)) ?? [];
  const book = evt.target.closest('js-product');
  const bookId = book.dataset.id;
- const currenBook = books.find(({ id }) => id === bookId);
- book.push(currenBook);
+ const currentBook = books.find(({ id }) => id === bookId);
+ book.push(currentBook);
  localStorage.setItem(BOOK_LS_KEY, JSON.stringify(books));
 }
 
-function handlerremoveLs() {
+function handlerremoveLs(evt) {
     if (!evt.target.classList.contains('js-remove')) {
     return;
     }
     const books = JSON.parse(localStorage.getItem(BOOK_LS_KEY)) ?? [];
     const book = evt.target.closest('js-product');
     const bookId = book.dataset.id;
-    const currenBook = books.find(({ id }) => id === bookId);
-    book.push(currenBook);
+    const currentBook = books.find(({ id }) => id === bookId);
+    book.push(currentBook);
     localStorage.setItem(BOOK_LS_KEY, JSON.stringify(books));
    }
