87613be77e9c5f18c657cbabb3f7fc3c4228d7f9
yaroslawv-nexus <124203359+yaroslawv-nexus@users.noreply.github.com>
2023-08-09 19:09:31 +0300

    invis scroll add container

--- src/js/book-list.js
+++ src/js/book-list.js
@@ -1,5 +1,6 @@
-
 import { getCategotyList } from './bookAPI'
+import { onClickCategory } from './renderSelectCategory'
+import { renderCategory } from './bestBooks'
 
 const list = document.querySelector('.book-list-ul');
 
@@ -18,7 +19,6 @@ function createMarkup(list) {
                      <a href="" class="book-list-link"><span>${element.list_name}</span></a>
                 </li>`
     }).join('');
-    console.log(result);
     return result;
 }
 
@@ -31,13 +31,22 @@ list.addEventListener('click', addClass);
 
 function addClass(evt) {
     evt.preventDefault();
-    if (evt.target.nodeName === "SPAN") {
-        const removeEl = document.querySelector('.current');
-        removeEl.classList.remove('current');
-        const currentEl = evt.target;
-        currentEl.classList.add('current');
+    if (evt.target.nodeName !== "SPAN") {
+        return;
+    }
+    const removeEl = document.querySelector('.current');
+    removeEl.classList.remove('current');
+    const currentEl = evt.target;
+    currentEl.classList.add('current');
+
+    if (evt.target.textContent === "All categories") {
+        renderCategory();
+    } else {
+        onClickCategory(evt.target.textContent);
     }
 
 }
+
+
 
 
