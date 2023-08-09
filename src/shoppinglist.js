d70ab9315d8d4de1eec701b934fe3ea44274200f
katesyvash795 <126354194+katesyvash795@users.noreply.github.com>
2023-08-09 20:56:45 +0300

    shopping-list/theme_dark+theme_dark_modal

--- src/shoppinglist.js
+++ src/shoppinglist.js
@@ -2,7 +2,6 @@ import './js/scroll-up';
 import './js/support';
 import './js/header';
 import './js/theme-switch';
-
 const goToCartButton = document.querySelector(".js-go-to-cart");
 const refJsLIst =document.querySelector(".js-list");
 const container=document.querySelector(".js-list");
@@ -16,12 +15,12 @@ function createMarkup(arr) {
             <div class="top-item-div">
                 <h2 class="shopping-list-title">${title}</h2>
                 <button class="js-remove" data-id="${title}">
-                    <svg width="22" height="22" class="shop-icons">
-                        <use href="../images/icons.svg#icon-trash"></use>
-                    </svg>
+                    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M6 2H10M2 4H14M12.6667 4L12.1991 11.0129C12.129 12.065 12.0939 12.5911 11.8667 12.99C11.6666 13.3412 11.3648 13.6235 11.0011 13.7998C10.588 14 10.0607 14 9.00623 14H6.99377C5.93927 14 5.41202 14 4.99889 13.7998C4.63517 13.6235 4.33339 13.3412 4.13332 12.99C3.90607 12.5911 3.871 12.065 3.80086 11.0129L3.33333 4M6.66667 7V10.3333M9.33333 7V10.3333" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
                 </button>
             </div>
-            <p class="shopping-list-text">${limitWords(description, 4)}</p>
+            <p class="shopping-list-text">${limitWords(description, 18)}</p>
             <div class="author-icons">
                 <p class="shopping-list-author">${author}</p>
                 <div class="book-links">
@@ -34,13 +33,18 @@ function createMarkup(arr) {
 }
 
 function createBookLinks(buy_links) {
-    return buy_links.map(link => `<a href="${link.url}" target="_blank"> <img src="${link.icon}" alt="Link Icon" class="link-icon"></a>`).join("");
+    return `<a href="${buy_links[0].url}"> <img src = "/amazon.ecd8e7ff.png" alt="Link Icon" class="link-icon amazon" width="32" height="11"></a>
+    <a href="${buy_links[1].url}"> <img src = "/apple-books.1d372193.png" alt="Link Icon" class="link-icon apple-book" width="16" height="16"></a>
+    <a href="${buy_links[2].url}"> <img src = "/book-shop.10ac238f.png" alt="Link Icon" class="link-icon book-shop" width="16" height="16"></a>`
+   
 }
 console.log(createMarkup(products));
 if (localStorage.getItem('shoppingList') === null) {
     // Если данных нет, выводим сообщение "Книг нет"
     const hiddenElement = document.querySelector('.is-hidden');
+    const hidden = document.querySelector('.cart');
     hiddenElement.classList.remove('is-hidden');
+    hidden.classList.add('cart');
 } 
 else {
     // Если данные уже есть в локальном хранилище, используем их
@@ -61,7 +65,11 @@ function removeProduct(event) {
     updateLocalStorage();
      // Оновити локальне сховище після видалення
      if(!document.querySelector(".shopping-list-item")){
-        refJsLIst.innerHTML="This page is empty, add some books and proceed to order.";
+        const hiddenElement = document.querySelector('.is-hidden');
+        const hidden = document.querySelector('.cart');
+        hiddenElement.classList.remove('is-hidden');
+        hidden.classList.add('cart');
+        // refJsLIst.innerHTML="This page is empty, add some books and proceed to order.";
      }
   }
 }
